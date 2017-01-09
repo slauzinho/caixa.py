23d34cff1e564cd84688c9383f0207fb73da1a65
teerryn <slaughterer90@gmail.com>
2017-01-09 17:54:38 +0000

    No longer needed

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 # Here you should enter all your account details for the application to be
 # able to login and select which account number you want to parse the data
 
-USERNAME = "XXXXXXX"
-PASSWORD = "XXXXXXX"
 ACCOUNT_NR = "XXXXXXXX" # Format "PT 00350XXXXXXXXXXXXXXXXEUR0"
 
 # Here we will config the pushbullet application
